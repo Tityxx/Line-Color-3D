21a9df91c5bb937e0d16f474aebff506b860966a
Nikita.Sukhodub <syhodyb99@gmail.com>
2022-07-17 20:16:07 +0700

    Import Windows Controller

---
+++ Assets/ImportedAssets/Tools And Mechanics/User Interface Manager/Scripts/OpenWindowButton.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Zenject;
+
+namespace ToolsAndMechanics.UserInterfaceManager
+{
+    /// <summary>
+    /// Кнопка открытия окна
+    /// </summary>
+    public class OpenWindowButton : AbstractButton
+    {
+        [SerializeField]
+        private WindowData window;
+        [SerializeField]
+        private bool closeCurrentWindow;
+
+        [Inject]
+        private WindowsController controller;
+
+        protected override void OnEnable()
+        {
+            base.OnEnable();
+            btn.interactable = true;
+        }
+
+        public override void OnButtonClick()
+        {
+            if (closeCurrentWindow)
+                btn.interactable = false;
+            controller.SetWindow(window, closeCurrentWindow);
+        }
+    }
+}
